7d93b7d789f513a07164e02f1ecc132bb03df707
maximiloi <maximiloi@gmail.com>
2024-06-04 23:04:16 +0300

    refactor: scripts

--- scripts/st-parts.js
+++ scripts/st-parts.js
@@ -1,67 +1,32 @@
-function createLink(titleEl) {
-  const titleName = titleEl.textContent.trim();
-
-  const linkArmtecEl = document.createElement('a');
-  linkArmtecEl.innerText = 'Armtek';
-  linkArmtecEl.href = `https://armtek.ru/search?text=${titleName}`;
-  linkArmtecEl.target = '_blank';
-  linkArmtecEl.style.marginLeft = '0.5rem';
-  linkArmtecEl.style.fontWeight = '900';
-  linkArmtecEl.style.color = 'red';
-  titleEl.after(linkArmtecEl);
-
-  const linkIxoraEl = document.createElement('a');
-  linkIxoraEl.innerText = 'Ixora';
-  linkIxoraEl.href = `https://ixora-auto.ru/catalog-article?article=${titleName}`;
-  linkIxoraEl.target = '_blank';
-  linkIxoraEl.style.marginLeft = '0.5rem';
-  linkIxoraEl.style.fontWeight = '900';
-  linkIxoraEl.style.color = 'red';
-  titleEl.after(linkIxoraEl);
-
-  const linkAAPEl = document.createElement('a');
-  linkAAPEl.innerText = 'AllAutoParts';
-  linkAAPEl.href = `https://allautoparts.ru/${titleName}`;
-  linkAAPEl.target = '_blank';
-  linkAAPEl.style.marginLeft = '0.5rem';
-  linkAAPEl.style.fontWeight = '900';
-  linkAAPEl.style.color = 'red';
-  titleEl.after(linkAAPEl);
-
-  const linkRoscoEl = document.createElement('a');
-  linkRoscoEl.innerText = 'Rossko';
-  linkRoscoEl.href = `https://rossko.ru/search/?text=${titleName}&q=&t=d`;
-  linkRoscoEl.target = '_blank';
-  linkRoscoEl.style.marginLeft = '0.5rem';
-  linkRoscoEl.style.fontWeight = '900';
-  linkRoscoEl.style.color = 'red';
-  titleEl.after(linkRoscoEl);
-
-  const linkPKEl = document.createElement('a');
-  linkPKEl.innerText = 'ПартКом';
-  linkPKEl.href = `https://www.part-kom.ru/new/#/search/0/0/0/${titleName}`;
-  linkPKEl.target = '_blank';
-  linkPKEl.style.marginLeft = '0.5rem';
-  linkPKEl.style.fontWeight = '900';
-  linkPKEl.style.color = 'red';
-  titleEl.after(linkPKEl);
-
-  const linkGoogleEl = document.createElement('a');
-  linkGoogleEl.innerText = 'G';
-  linkGoogleEl.href = `https://www.google.com/search?q=${titleName}`;
-  linkGoogleEl.target = '_blank';
-  linkGoogleEl.style.marginLeft = '1rem';
-  linkGoogleEl.style.fontWeight = '900';
-  linkGoogleEl.style.color = 'red';
-  titleEl.after(linkGoogleEl);
+function createLink(titleEl, linkData) {
+  const linkEl = document.createElement('a');
+  linkEl.innerText = linkData.name;
+  linkEl.href = linkData.url;
+  linkEl.target = '_blank';
+  linkEl.style.marginLeft = linkData.marginLeft || '0.5rem';
+  linkEl.style.fontWeight = '900';
+  linkEl.style.color = 'red';
+  titleEl.after(linkEl);
 }
+
+const linkData = [
+  { name: 'Armtek', url: 'https://armtek.ru/search?text=' },
+  { name: 'Ixora', url: 'https://ixora-auto.ru/catalog-article?article=' },
+  { name: 'AllAutoParts', url: 'https://allautoparts.ru/' },
+  { name: 'Rossko', url: 'https://rossko.ru/search/?text=&q=&t=d' },
+  { name: 'ПартКом', url: 'https://www.part-kom.ru/new/#/search/0/0/0/' },
+  { name: 'G', url: 'https://www.google.com/search?q=', marginLeft: '1rem' },
+];
 
 setTimeout(function () {
   const titleElArr = document.querySelectorAll('span.part-article');
 
   if (titleElArr.length > 0) {
     titleElArr.forEach((titleEl) => {
-      createLink(titleEl);
+      const titleName = titleEl.textContent.trim();
+      linkData.forEach((link) => {
+        createLink(titleEl, { ...link, url: link.url + titleName });
+      });
     });
   } else {
     alert('Перезагрузите страницу, F5');
